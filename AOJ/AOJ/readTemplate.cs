9298bb8b38e7f1c4745682246c0cc9caa1c60c25
masayamatu <34940116+masayamatu@users.noreply.github.com>
2021-08-10 23:02:57 +0900

    R030810
    R030810

---
+++ AOJ/AOJ/readTemplate.cs
@@ -0,0 +1,24 @@
+/*public class Hello{
+    public static void Main(){
+        var line = System.Console.ReadLine();
+    }
+}
+public class Hello{
+    public static void Main(){
+        var line = int.Parse(System.Console.ReadLine());
+    }
+}
+public class Hello{
+    public static void Main(){
+        var line = System.Console.ReadLine().Split(' ');
+    }
+}
+public class Hello{
+    public static void Main(){
+        var line = System.Console.ReadLine().Split(' ');
+        int[] array = new int[line.Length];
+        for(int i=0; i<line.Length; i++){
+            array[i] = int.Parse(line[i]);
+        }
+    }
+}*/
